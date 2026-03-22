73a09339a1984a3ca2ca584971b92209da0164d5
Adithya R <143496292+THE-Adi-005@users.noreply.github.com>
2026-03-23 01:37:18 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -49,4 +49,4 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-st.text(smoothiefroot_response).json()
+st.text(smoothiefroot_response.json())
